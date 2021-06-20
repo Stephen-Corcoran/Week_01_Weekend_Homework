ab8df25be0c088ee416fc4c37da5c4978f68d376
Stephen <stephendcorcoran@gmail.com>
2021-06-20 14:03:44 +0100

    5 tasks completed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,8 +1,14 @@
-# WRITE YOUR FUNCTIONS HERE
-
 def get_pet_shop_name (pet_shop):
     return pet_shop ["name"]
 
-
 def get_total_cash (pet_shop):
     return pet_shop ["admin"]["total_cash"]
+
+def add_or_remove_cash(balance, cash):
+    balance ["admin"]["total_cash"] += cash
+
+def get_pets_sold (pet_shop):
+    return pet_shop ["admin"]["pets_sold"]
+
+def increase_pets_sold (sales, sold):
+    sales ["admin"]["pets_sold"] += sold
